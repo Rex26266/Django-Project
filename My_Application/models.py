8e875f82ef537e1f58e1b15bfc1d1a57e4426401
rex <rexmanickam@boscoits.com>
2022-04-12 16:09:10 +0530

    first commit

---
+++ My_Application/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Member(models.Model):
+    MEMBERSHIP_TYPE = (
+        ('RE', 'Religious'),
+        ('SE', 'Secular Clergy'),
+        ('LP', 'Lay Person'),
+    )
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    dob = models.DateField()
+    age = models.IntegerField()
+    membership_type = models.CharField(max_length=2, choices=MEMBERSHIP_TYPE)
+
+    def __str__(self):
+        return self.first_name
